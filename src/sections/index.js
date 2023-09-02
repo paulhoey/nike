81dcc3d7c274acb7c321a4ea1a8eaf9ec8097543
Paul Hoey <paulhoey@outlook.com>
2023-09-02 20:25:15 +0100

    init

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./customerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Hero from "./Hero";
+
+export {
+    Footer,
+    CustomerReviews,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
