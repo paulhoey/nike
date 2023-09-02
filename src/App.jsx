d35ce7f4a946e73ca678ece81e4b01e276353f5e
Paul Hoey <paulhoey@outlook.com>
2023-09-02 22:23:00 +0100

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Nav from "./components/Nav";
 const  App = () => (
   <main className="relative">
     <Nav />
-    <section className="xl:padding-1wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero/>
     </section >
     <section className="padding">
